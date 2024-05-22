df3d913d631183313177d89095176a1b071b4f4d
Elijah <xrealntrue16x@gmail.com>
2024-05-22 10:02:14 -0400

    Validation implemented for both forms

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,9 +1,40 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-function showInputError(formEl, input, config) {
-  const errorMessage = formEl.querySelector(`#${input.id}-error`);
-  console.log(errorMessage);
+function toggleButtonState(input, submitButton, config) {
+  let foundInvalid = false; //set var to false to begin
+  input.forEach((input) => {
+    if (!input.validity.valid) {
+      //if any input is not valid
+      foundInvalid = true; //set foundInvalid to true
+    }
+  });
+
+  if (foundInvalid) {
+    //if foundInvalid is true
+    submitButton.classList.add(inactiveButtonClass); //add disabled button class for visual effect
+    submitButton.disabled = true; //disable button
+  } else {
+    //if foundInvalid is false
+    submitButton.classList.remove(inactiveButtonClass); //remove disabled button class
+    submitButton.disabled = false; //enable button
+  }
+}
+
+function showInputError(formEl, input, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${input.id}-error`);
+  console.log(errorMessageEl);
+  input.classList.add(inputErrorClass); //Add inputErrorClass from config obj to input || {inputErrorClass} = conig.inputErrorClass
+  errorMessageEl.textContent = input.validationMessage; //set error text to validationMessage browser property
+  errorMessageEl.classList.add(errorClass); //Add Fade in animation for error
+}
+
+function hideInputError(formEl, input, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${input.id}-error`);
+  console.log(errorMessageEl);
+  input.classList.remove(inputErrorClass); //Add inputErrorClass from config obj to input || {inputErrorClass} = conig.inputErrorClass
+  errorMessageEl.textContent = " "; //reset error text to nothing
+  errorMessageEl.classList.remove(errorClass); //Add Fade in animation for error
 }
 
 function checkInputValidity(formEl, input, config) {
@@ -17,13 +48,14 @@ function checkInputValidity(formEl, input, config) {
 function setEventListeners(formEl, config) {
   //find all inputs in a form
   const { inputSelector } = config; // {inputSelector} is the same as const inputSelector = config.inputSelector
-  const inputElements = [...formEl.querySelectorAll(inputSelector)];
-
-  //for each input add an event listener and call checkInput
+  const inputElements = [...formEl.querySelectorAll(inputSelector)]; //get all inputs in formEl using spread operator and array
+  const submitButton = formEl.querySelector(".modal__button"); //get all modal submit buttons
+  //for each input add an event listener and call checkInput and toggleButtonState
   inputElements.forEach((input) => {
     input.addEventListener("input", (e) => {
       console.log(input.validationMessage);
       checkInputValidity(formEl, input, config);
+      toggleButtonState(input, submitButton);
     });
   });
 }
