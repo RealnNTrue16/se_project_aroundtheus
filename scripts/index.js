02d2c4818a1cd3d7731d0bff809d907a3e179133
Elijah <xrealntrue16x@gmail.com>
2024-05-23 23:10:54 -0400

    Error Message working

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,33 @@
-//Create An array of objects
-
+// Create an array of objects
 const initialCards = [
-  //Obj 1
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
-
   {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
-
   {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
-
   {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
-
   {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
-
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-//Add Modals
-//Declare Profile Modal Variables
+
+// Add Modals
+// Declare Profile Modal Variables
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileModal = document.querySelector("#profile__edit-modal");
 const profileModalCloseButton = profileModal.querySelector(".modal__close");
@@ -45,39 +39,33 @@ const profileDescriptionInput = document.querySelector(
 );
 const profileEditForm = profileModal.querySelector(".modal__form");
 const profileModalOverlay = document.querySelector("#profile__edit-modal");
-console.log(profileModalOverlay);
 
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 const cardListElement = document.querySelector(".cards__list");
 
-//Add Card Modal
+// Add Card Modal
 const addCardModal = document.querySelector("#card__add-modal");
 const addNewCardButton = document.querySelector(".profile__add-button");
 const addCardModalCloseButton = addCardModal.querySelector(".modal__close");
 const addCardFormElement = addCardModal.querySelector(".modal__form");
 const cardTitleInput = addCardModal.querySelector("#card__title-input");
 const cardURLInput = addCardModal.querySelector("#card__description-input");
-//End Add Card Modal Vars
 
-//Modal Overlay
+// Modal Overlay
 const addCardModalOverlay = document.querySelector("#card__add-modal");
-console.log(addCardModalOverlay);
 
-//Preview Image Modal
+// Preview Image Modal
 const previewImageModal = document.querySelector("#preview__modal");
 const previewImageModalCloseButton =
   previewImageModal.querySelector(".modal__close");
-
 const previewImageModalOverlay = document.querySelector("#preview__modal");
 
-//FUNCTIONS
+// FUNCTIONS
 
 function handleOverlayClick(evt) {
-  console.log(evt);
   if (evt.target.classList.contains("modal")) {
-    //If target object has class 'modal'
-    handleModalClose(evt.target); //Call modal close function on target object
+    handleModalClose(evt.target);
   }
 }
 
@@ -97,7 +85,6 @@ function openModal(modal) {
 
 function handleProfileModalSubmit(evnt) {
   evnt.preventDefault();
-  // console.log("Works!");
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   handleModalClose(profileModal);
@@ -105,13 +92,10 @@ function handleProfileModalSubmit(evnt) {
 
 function handleAddCardFormSubmit(evnt) {
   evnt.preventDefault();
-  // console.log("Works!");
   const name = cardTitleInput.value;
   const alt = cardTitleInput.value;
   const link = cardURLInput.value;
   renderCard({ name, link, alt });
-
-  // Clear Inputs
   cardTitleInput.value = "";
   cardURLInput.value = "";
   handleModalClose(addCardModal);
@@ -119,11 +103,8 @@ function handleAddCardFormSubmit(evnt) {
 
 function handleModalClose(modal) {
   modal.classList.remove("modal_open");
-  //remove event listener
   modal.removeEventListener("click", handleOverlayClick);
   modal.removeEventListener("keydown", handleOverlayClick);
-
-  //Add Listeners
   modal.addEventListener("click", handleOverlayClick);
   modal.addEventListener("keydown", handleOverlayClick);
 }
@@ -134,27 +115,17 @@ function renderCard(cardData) {
 }
 
 function getCardElement(cardData) {
-  // 'card data' is an argument for InitialCards obj
-  //Clone the template and store it in a card element
   const cardElement = cardTemplate.cloneNode(true);
-
-  //access card title and image and store them in variables
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
-
-  //find delete button
   const deleteButton = cardElement.querySelector(".card__delete");
   deleteButton.addEventListener("click", () => {
     cardElement.remove();
   });
-
-  //Add Like Button Feature While cards are Rendered
   const likeButton = cardElement.querySelector(".card__like-button");
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
   });
-
-  //cardImage click listener
   cardImageElement.addEventListener("click", () => {
     const modalImage = previewImageModal.querySelector(".modal__image");
     const modalImageHeading = previewImageModal.querySelector(
@@ -163,58 +134,46 @@ function getCardElement(cardData) {
     modalImage.src = cardData.link;
     modalImageHeading.textContent = cardData.name;
     modalImage.alt = cardData.name;
-
-    //console.log(modalImage);
     openModal(previewImageModal);
   });
-
-  //set the path to the image to the link field of the object
   cardImageElement.src = cardData.link;
-  //set the image alt text
   cardImageElement.alt = cardData.name;
-
-  //set card title to name of object (initialCards) field
   cardTitleElement.textContent = cardData.name;
-
-  //return the ready HTML element with the filled in data
   return cardElement;
 }
 
-//EVENT LISTENERS
-//Profile Modal
+// EVENT LISTENERS
+
+// Profile Modal
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   openModal(profileModal);
 });
 
-//Profile Modal CLose Functions
+// Profile Modal Close Functions
 profileModalCloseButton.addEventListener("click", () =>
   handleModalClose(profileModal)
 );
 profileModal.addEventListener("click", handleOverlayClick);
-
 profileEditForm.addEventListener("submit", handleProfileModalSubmit);
 
-//Add New Card Modal
+// Add New Card Modal
 addNewCardButton.addEventListener("click", () => openModal(addCardModal));
-
-addCardModalCloseButton.addEventListener("click", () => {
-  handleModalClose(addCardModal);
-});
+addCardModalCloseButton.addEventListener("click", () =>
+  handleModalClose(addCardModal)
+);
 addCardModal.addEventListener("click", handleOverlayClick);
-
 addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+//console.log(addCardFormElement);
 
-//Close preview Modal
-previewImageModalCloseButton.addEventListener("click", () => {
-  handleModalClose(previewImageModal);
-});
+// Close Preview Modal
+previewImageModalCloseButton.addEventListener("click", () =>
+  handleModalClose(previewImageModal)
+);
 previewImageModal.addEventListener("click", handleOverlayClick);
 
-///////////
-
-//Iterate thru initialCards Object using 'forEach' or a for loop
+// Render Initial Cards
 initialCards.forEach((cardData) => {
   renderCard(cardData);
 });
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,19 +1,17 @@
-// enabling validation by calling enableValidation()
-// pass all the settings on call
-
 function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  console.log(inputEl);
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
   console.log(errorMessageEl);
-  inputEl.classList.add(inputErrorClass); //Add inputErrorClass from config obj to input || {inputErrorClass} = conig.inputErrorClass
-  errorMessageEl.textContent = inputEl.validationMessage; //set error text to validationMessage browser property
-  errorMessageEl.classList.add(errorClass); //Add Fade in animation for error
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
 }
 
 function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.remove(inputErrorClass); //Add inputErrorClass from config obj to input || {inputErrorClass} = conig.inputErrorClass
-  errorMessageEl.textContent = " "; //reset error text to nothing
-  errorMessageEl.classList.remove(errorClass); //Add Fade in animation for error
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = " ";
+  errorMessageEl.classList.remove(errorClass);
 }
 
 function checkInputValidity(formEl, inputEl, config) {
@@ -29,71 +27,54 @@ function toggleButtonState(
   submitButton,
   { inactiveButtonClass }
 ) {
-  const isFormValid = inputElements.every((input) => input.validity.valid); //check if each input is valid
+  const isFormValid = inputElements.every((input) => input.validity.valid);
   if (!isFormValid) {
-    //if NOT valid
-    submitButton.classList.add(inactiveButtonClass); // Add disabled button class
-    submitButton.disabled = true; // Disable button
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
   } else {
-    //If valid
-    submitButton.classList.remove(inactiveButtonClass); // Remove disabled button class
-    submitButton.disabled = false; // Enable button
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
   }
 }
 
 function handleInputEvent(formEl, inputEl, config) {
   checkInputValidity(formEl, inputEl, config);
   const { inputSelector, submitButtonSelector } = config;
-  const inputElements = [...formEl.querySelectorAll(inputSelector)]; //get all inputs in formEl using spread operator to turn them into an array
-  const submitButton = formEl.querySelector(submitButtonSelector); //get all modal submit buttons
-  toggleButtonState(inputElements, submitButton, config); //call this function to change button sate
+  const inputElements = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(submitButtonSelector);
+  toggleButtonState(inputElements, submitButton, config);
 }
 
 function setEventListeners(formEl, config) {
-  //find all inputs in a form
-  const { inputSelector } = config; // {inputSelector} is the same as const inputSelector = config.inputSelector
-  const inputElements = [...formEl.querySelectorAll(inputSelector)]; //get all inputs in formEl using spread operator and array
-  //for each input add an event listener and call checkInput and toggleButtonState
+  const { inputSelector, submitButtonSelector } = config;
+  const inputElements = [...formEl.querySelectorAll(inputSelector)];
   inputElements.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, config);
+      const submitButton = formEl.querySelector(submitButtonSelector); // Changed from popup to modal
       toggleButtonState(inputElements, submitButton, config);
     });
   });
 }
 
 function enableValidation(config) {
-  //get all form elements
-  const formElements = [...document.querySelectorAll(config.formSelector)]; //Make an array of each form element using array and spread operator
+  const formElements = [...document.querySelectorAll(config.formSelector)];
+  //console.log(formElements);
   formElements.forEach((formEl) => {
-    //for each form element
-
     formEl.addEventListener("submit", (e) => {
-      //add event listener
-      e.preventDefault(); //prevent page reload
+      e.preventDefault();
     });
-    //loop for all inputs inside of form
     setEventListeners(formEl, config);
-    //loop thru all inputs to check if all are valid
-
-    //if input is not valid
-    //grab validation message
-    //add error class to input
-    //display error message
-    //disable button
-    //if inputs are valid
-    //enable button
-    //reset error messages
   });
 }
 
 const config = {
-  formSelector: ".modal__popup__form",
-  inputSelector: ".modal__popup__input",
-  submitButtonSelector: ".modal__popup__button",
-  inactiveButtonClass: "modal__popup__button_disabled",
-  inputErrorClass: "modal__popup__input_type_error",
-  errorClass: "modal__popup__error_visible",
+  formSelector: ".modal__popup__form", // Updated
+  inputSelector: ".modal__popup__input", // Updated
+  submitButtonSelector: ".modal__popup__button", // Updated
+  inactiveButtonClass: "modal__popup__button_disabled", // Updated
+  inputErrorClass: "modal__popup__input_type_error", // Updated
+  errorClass: "modal__popup__error_visible", // Updated
 };
 
 enableValidation(config);
