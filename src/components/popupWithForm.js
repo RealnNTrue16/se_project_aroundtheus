895e35655367448802f595f2b6cf577109afa642
Elijah <xrealntrue16x@gmail.com>
2024-06-26 20:46:38 -0400

    PopupForm child class created

--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -4,11 +4,31 @@ class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".modal __form");
-    this._handleFormSubmit = document.querySelector(handleFormSubmit);
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  _getInputValues() {
+    const formInputs = [
+      ...this._popupForm.querySelectorAll(".modal__form-input"),
+    ];
+    const inputsObj = {}; //create empty object
+    formInputs.forEach((inputEl) => {
+      //forEach inputEl
+      inputsObj[inputEl.name] = inputEl.value; //set object name to inputel.name and values to inputel value
+    });
+    return inputsObj;
+  }
+
+  setEventListeners() {
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault(); //prevent page refresh
+      this._handleFormSubmit();
+    });
+    super.setEventListeners();
   }
 
   close() {
     this._popupForm.reset(); //reset form fields
-    super.closepopup(); //close popup by calling parent class close method
+    super.closePopup(); //close popup by calling parent class close method
   }
 }
