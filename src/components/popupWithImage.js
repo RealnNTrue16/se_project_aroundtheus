37b7764d6c74c8f0399d8c878e44546311854e8f
Elijah <xrealntrue16x@gmail.com>
2024-06-27 21:32:20 -0400

    Fixed Page not loading

--- src/components/popupWithImage.js
+++ src/components/popupWithImage.js
@@ -1,10 +1,12 @@
 import Popup from "./popup";
 
 export default class popupWithImage extends Popup {
-  constructor({ popupSelector }) {
+  constructor(popupSelector) {
     super({ popupSelector }); //call parent constructor
-    this._popupImage = this._popupElement.querySelector(".card__image");
-    this._popupImageText = this._popupElement.querySelector(".card__title");
+    this._popupImage = this._popupElement.querySelector(".modal__image");
+    this._popupImageText = this._popupElement.querySelector(
+      ".modal__image_heading"
+    );
   }
 
   open({ name, link }) {
