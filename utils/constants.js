2775d9b3063d27d881bf9ded32d0182064606be3
Elijah <xrealntrue16x@gmail.com>
2024-06-28 23:31:07 -0400

    utils folder created

---
+++ utils/constants.js
@@ -0,0 +1,37 @@
+// Create an array of objects
+export const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+//Config settings
+export const config = {
+  formSelector: ".modal__popup__form", // Updated
+  inputSelector: ".modal__popup__input", // Updated
+  submitButtonSelector: ".modal__popup__button", // Updated
+  inactiveButtonClass: "modal__popup__button_disabled", // Updated
+  inputErrorClass: "modal__popup__input_type_error", // Updated
+  errorClass: "modal__popup__error_visible", // Updated
+};
