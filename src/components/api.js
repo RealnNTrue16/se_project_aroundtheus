71c63b2043ac604dcdbb5019732605419000206a
Elijah <xrealntrue16x@gmail.com>
2024-07-15 15:03:13 -0400

    API class created

---
+++ src/components/api.js
@@ -0,0 +1,34 @@
+export default class Api {
+  constructor(options) {}
+
+  //methods
+  getInitialCards() {
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "4792ec92-cf1c-45a6-8740-0f5d63585faa",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          //check for successful response
+          console.log("Respose:" + res);
+          return res.json();
+        }
+        //check for unsuccessful response
+        console.log("Error");
+        return Promise.reject(`Error: ${res.status}`);
+      }) //process errors
+      .catch((err) => console.error(err));
+  }
+
+  getUserInfo() {
+    return fetch(
+      "GET https://around-api.en.tripleten-services.com/v1/users/me",
+      {
+        headers: {
+          authorization: "4792ec92-cf1c-45a6-8740-0f5d63585faa",
+        },
+      }
+    );
+  }
+}
